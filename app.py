5d59dbb883668ae86be0aaa6714813352dfc194a
idokoHC <idokohc@gmail.com>
2026-02-02 07:45:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,37 @@ st.image("https://studentroom.co.za/wiki/wp-content/uploads/2020/12/TUT-logo-102
 st.header("Student Profile")
 
 # Collect basic information
-name = "Engr. Dr Hillary Chika Idoko"
-field = "Electrical Engineering"
-institution = "University of Nigeria Nsukka"
-
-
+name = "Engr. Hillary Chika Idoko"
+level = "Doctor of Engineering) 
+specialization = "Electric Machines and Drives"
+department = "Electrical Engineering"
+faculty = "Engineering and Built Environment" 
+institution = "Tshwane University of Technology"
 st.write(f"**Name:** {name}")
-st.write(f"**Field of Research:** {field}")
+st.write(f"**Level of Study:** {level}")
+st.write(f"**Area of specialization:** {specialization}")
+st.write(f"**Department:** {department}")
+st.write(f"**Faculty:** {faculty}")
 st.write(f"**Institution:** {institution}")
 
-# Add a section for publications
-st.header("Publications")
+# Add a section for research interests
+st.header("Research Interest")
+st.write("Electric Machines and Drives")
+st.write("Power Electronics")
+st.write("Renewable Energy Technology")
+
+# Add a section for computer programming skills
+st.header("Computer Programming Skills")
+st.write("Python")
+st.write("Linux/Bash")
+st.write("ANSYS Maxwell")
+st.write("MATLAB/Simulink")
 
 # Add a contact section
 st.header("Contact Information")
-email = "jane.doe@example.com"
+email = "222449294@tut4life.ac.za"
 st.write(f"You can reach {name} at {email}.")
+
 
 
 
